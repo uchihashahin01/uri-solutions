8282de4e527533c8679d44341547b9833da79cc9
UchihaShahin <alam15-3186@diu.edu.bd>
2022-09-03 22:04:01 +0600

    PPSUM

--- CodeChef/Puppy_and_Sum.py
+++ CodeChef/Puppy_and_Sum.py
@@ -1,5 +1,8 @@
 def my_func(a,b):
-    print(sum(a)+sum(b))
+    for _ in range(a):
+        b = (b *(b +1)) // 2
+    print(b)
+
 
 for _ in range(int(input())):
     a,b = list(map(int,input().split()))
