72099c4acf5c43a32968159e1e5f505cf32751c5
UchihaShahin <alam15-3186@diu.edu.bd>
2022-09-07 21:01:14 +0600

    FEVER

---
+++ CodeChef/Broken_Phone.py
@@ -0,0 +1,9 @@
+for i in range(int(input())):
+    a,b = list(map(int,input().split()))
+    
+    if a < b:
+        print("REPAIR")
+    elif a == b:
+        print("ANY")
+    else:
+        print("NEW PHONE")
---
+++ CodeChef/Fever.py
@@ -0,0 +1,7 @@
+for i in range(int(input())):
+    a = int(input())
+
+    if a > 98:
+        print("YES")
+    else:
+        print("NO")
